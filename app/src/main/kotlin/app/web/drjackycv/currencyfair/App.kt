4215bde4523f18c256e0ef4c4f266d19fc3dfd68
Hossein <hossein@safeboda.com>
2019-09-23 18:48:34 +0200

    Create app class

---
+++ app/src/main/kotlin/app/web/drjackycv/currencyfair/App.kt
@@ -0,0 +1,13 @@
+package app.web.drjackycv.currencyfair
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
