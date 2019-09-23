120510323af78cfdeed6c6b60e32b1f1246d040a
Hossein <hossein@safeboda.com>
2019-09-23 21:19:56 +0200

    Add default query params to the interceptor

--- data/src/main/kotlin/app/web/drjackycv/data/network/TokenInterceptor.kt
+++ data/src/main/kotlin/app/web/drjackycv/data/network/TokenInterceptor.kt
@@ -1,6 +1,11 @@
 package app.web.drjackycv.data.network
 
-import app.web.drjackycv.data.network.constant.DataConstants.ACCESS_KEY
+import app.web.drjackycv.data.network.constant.DataConstants.ACCESS_KEY_KEY
+import app.web.drjackycv.data.network.constant.DataConstants.ACCESS_KEY_VALUE
+import app.web.drjackycv.data.network.constant.DataConstants.NO_JSON_CALLBACK_KEY
+import app.web.drjackycv.data.network.constant.DataConstants.NO_JSON_CALLBACK_VALUE
+import app.web.drjackycv.data.network.constant.DataConstants.REQUEST_FORMAT_KEY
+import app.web.drjackycv.data.network.constant.DataConstants.REQUEST_FORMAT_VALUE
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -10,8 +15,12 @@ class TokenInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val httpUrl = originalRequest.url
-        val newHttpUrl = httpUrl.newBuilder().addQueryParameter("userKey", ACCESS_KEY)
-            .build() //TODO: Request automatically for new token when this one expired
+        val newHttpUrl = httpUrl.newBuilder()
+            //TODO: Request automatically for new token when this one expired
+            .addQueryParameter(ACCESS_KEY_KEY, ACCESS_KEY_VALUE)
+            .addQueryParameter(REQUEST_FORMAT_VALUE, REQUEST_FORMAT_KEY)
+            .addQueryParameter(NO_JSON_CALLBACK_KEY, NO_JSON_CALLBACK_VALUE)
+            .build()
         val requestBuilder = originalRequest.newBuilder().url(newHttpUrl)
         val request = requestBuilder.build()
 
--- data/src/main/kotlin/app/web/drjackycv/data/network/constant/DataConstants.kt
+++ data/src/main/kotlin/app/web/drjackycv/data/network/constant/DataConstants.kt
@@ -1,5 +1,10 @@
 package app.web.drjackycv.data.network.constant
 
 object DataConstants {
-    const val ACCESS_KEY = "e912c9fa2ff261c782e9449b87985bab"
+    const val ACCESS_KEY_KEY = "api_key"
+    const val ACCESS_KEY_VALUE = "e912c9fa2ff261c782e9449b87985bab"
+    const val REQUEST_FORMAT_KEY = "format"
+    const val REQUEST_FORMAT_VALUE = "json"
+    const val NO_JSON_CALLBACK_KEY = "nojsoncallback"
+    const val NO_JSON_CALLBACK_VALUE = "1"
 }
