472de218d5068dced3f34583c751f7208838fba9
Dr.jacky <dr.jacky.2005@gmail.com>
2019-11-23 12:07:41 +0100

    Implement basic class for handling failures

--- domain/src/main/kotlin/app/web/drjackycv/domain/entity/base/Failure.kt
+++ domain/src/main/kotlin/app/web/drjackycv/domain/entity/base/Failure.kt
@@ -3,21 +3,23 @@ package app.web.drjackycv.domain.entity.base
 private const val UNKNOWN_CODE = -1
 private const val UNKNOWN_STAT = "UNKNOWN STAT"
 
-sealed class Failure(var retryAction: () -> Unit) : Throwable() {
+sealed class Failure : Throwable() {
 
-    abstract class FailureWithMessage(override val message: String) : Failure({})
+    abstract class FailureWithMessage(open val msg: String) : Failure()
 
     class Error(val stat: String, val code: Int, override val message: String) :
         FailureWithMessage(message) {
         constructor(message: String) : this(UNKNOWN_STAT, UNKNOWN_CODE, message)
     }
 
-    class Timeout(override val message: String) : FailureWithMessage(message)
+    class Timeout(override val msg: String) : FailureWithMessage(msg)
 
-    class NoInternet(override val message: String) : FailureWithMessage(message)
+    class NoInternet(override val msg: String) : FailureWithMessage(msg)
 
-    object Unauthorized : Failure({})
+    class Unauthorized(override val msg: String) : FailureWithMessage(msg)
 
-    object NotFound : Failure({})
+    class UnKnown(override val msg: String) : FailureWithMessage(msg)
+
+    class NotFound(override val msg: String) : FailureWithMessage(msg)
 
 }
--- presentation/src/main/kotlin/app/web/drjackycv/presentation/ui/base/viewmodel/BaseViewModel.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/ui/base/viewmodel/BaseViewModel.kt
@@ -7,14 +7,13 @@ import androidx.lifecycle.ViewModel
 import app.web.drjackycv.domain.entity.base.Failure
 import app.web.drjackycv.presentation.BuildConfig
 import app.web.drjackycv.presentation.R
-import dagger.Lazy
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 abstract class BaseViewModel : ViewModel() {
 
     @Inject
-    lateinit var resources: Lazy<Resources>
+    lateinit var resources: Resources
 
     private var mutableFailure: MutableLiveData<Failure> = MutableLiveData()
     val ldFailure: LiveData<Failure> = mutableFailure
@@ -28,26 +27,25 @@ abstract class BaseViewModel : ViewModel() {
         super.onCleared()
     }
 
-    protected fun getFailure(throwable: Throwable, retryAction: () -> Unit): Failure {
+    protected fun getFailure(throwable: Throwable): Failure {
         val failure = throwable as? Failure ?: Failure.Error(
             if (throwable.message?.isNotEmpty() == true) {
                 @Suppress("ConstantConditionIf")
                 if (BuildConfig.BUILD_TYPE == "release") {
-                    resources.get().getString(R.string.unknown_error)
+                    resources.getString(R.string.unknown_error)
                 } else {
-                    throwable.message ?: resources.get().getString(R.string.unknown_error)
+                    throwable.message ?: resources.getString(R.string.unknown_error)
                 }
             } else {
-                resources.get().getString(R.string.unknown_error)
+                resources.getString(R.string.unknown_error)
             }
         )
 
-        failure.retryAction = retryAction
         return failure
     }
 
-    protected fun handleFailure(throwable: Throwable, retryAction: () -> Unit) {
-        val failure = getFailure(throwable, retryAction)
+    protected fun handleFailure(throwable: Throwable) {
+        val failure = getFailure(throwable)
         mutableFailure.value = failure
     }
 
